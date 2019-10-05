8996331a8a10a5d5ab8da852d5eba6843a793f66
huangyiping <hwangyiping@163.com>
2019-10-05 17:36:19 +0800

    Initial commit

---
+++ api/controllers/RentalInfoController.js
@@ -0,0 +1,12 @@
+/**
+ * RentalInfoController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
